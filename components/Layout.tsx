b20ce11139efd947d762fe78f4540e311f2b531d
Vitoria Lopes <heyvitoria.lopes@gmail.com>
2021-01-23 22:12:33 -0300

    finished project

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,12 @@
-import Header from './Header'
+import Header from "./Header";
 
 const Layout: React.FC = ({ children }) => {
-  return (
-    <div className="mx-auto max-w-5xl px-4 sm:px-6 md:px-8">
-      <Header />
-      {children}
-    </div>
-  )
+   return (
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-8">
+         <Header />
+         {children}
+      </div>
+   )
 }
 
-export default Layout
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
-import { AnimateSharedLayout } from 'framer-motion'
-
 import '../styles/tailwind.css'
 import '../styles/globals.css'
-
 import Layout from '../components/Layout'
+import { AnimateSharedLayout } from 'framer-motion'
 
 function MyApp({ Component, pageProps }) {
   return (
